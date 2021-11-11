ef80bec5593647bb2d888e265483515fa45a925b
IsraelSanchezM <80420853+IsraelSanchezM@users.noreply.github.com>
2021-11-11 16:50:36 -0600

    Actualización de LimpiadorModel.py
    Se actualizó el programa para que las celdas sucias fueran una clase a parte.

--- LimpiadorModel.py
+++ LimpiadorModel.py
@@ -19,40 +19,59 @@ from mesa.time import SimultaneousActivation
 from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 
-def movimientos_agentes(model):
-    """Función que suma todos los movimientos de los agentes"""
+def movimientosAgentes(model):
+    """
+    Función que suma todos los movimientos de los agentes
+    Parámetros: 
+    model - modelo al que pertenece la función
+    Retornos:
+    sum(movAgentes) - sumatoria de todos los movimientos de los agentes
+    """
     #Se crea un arreglo con todos los movimientos de todos los agentes
-    mov_agentes = [agent.movimientos for agent in model.schedule.agents]
+    movAgentes = [agent.movimientos for agent in model.schedule.agents]
     #Se regresa la suma de todos los movimientos
-    return sum(mov_agentes)
+    return sum(movAgentes)
 
-def calc_celdas_limpias(model):
-    """Función que calcula el procentaje de celdas limpias"""
+def calcCeldasLimpias(model):
+    """
+    Función que calcula el procentaje de celdas limpias
+    Parámetros: 
+    model - modelo al que pertenece la función
+    Retornos:
+    cells - porcentaje de celdas limpias
+    """
     #Se le resta al 100% el porcentaje no limpio
-    cells = 100 - (len(model.dirty_cells) * model.cells / 100)
+    cells = 100 - (len(model.dirtyCells) * 100/ model.cells)
     #Se regresa el procentaje de celdas limpias
     return cells
 
-def calc_tiempo(model):
-    """Función que calcula el tiempo (en pasos) usado hasta ahora"""
+def calcTiempo(model):
+    """
+    Función que calcula el tiempo (en pasos) usado hasta ahora
+    Parámetros: 
+    model - modelo al que pertenece la función
+    Retornos:
+    model.schedule.time - tiempo (en pasos) que tiene el modelo actualmente
+    """
     return model.schedule.time
     
 
 class LimpiadorModel(Model):
     """Modelo de los limpiadores"""
     def __init__(self, N, width, height, percent, time):
-        self.num_agents = N
+        #Atributos
+        self.numAgents = N
         self.grid = MultiGrid(width, height, True)
         self.cells = width * height
-        self.dirty_cells = []
+        self.dirtyCells = []
         self.schedule = SimultaneousActivation(self)
         self.percent = percent
         self.time = time
         self.running = True
 
         #Se crean los agentes
-        for i in range(self.num_agents):
-            limpiador = AgenteLimpiador("Limpiador " + str(i), self, 0)
+        for i in range(self.numAgents):
+            limpiador = AgenteLimpiador("Limpiador " + str(i), self)
             self.schedule.add(limpiador)
 
             #Agregar a todos los agentes en la celda[1][1]
@@ -65,24 +84,31 @@ class LimpiadorModel(Model):
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             temp = (x, y)
-            if(temp not in self.dirty_cells):
+            if(temp not in self.dirtyCells):
                 #Si no existen las coordenadas en el arreglo de celdas sucias se agregan
-                self.dirty_cells.append(temp)
+                self.dirtyCells.append(temp)
                 #Se crea un agente de tipo suciedad, se agrega al schedule y al tablero
-                dirt = AgenteLimpiador(dirty, self, 1)
+                dirt = Suciedad(dirty, self)
                 self.schedule.add(dirt)
                 self.grid.place_agent(dirt, temp)
                 dirty -= 1
 
+        #Se inicializa el collector de datos del modelo
         self.datacollector = DataCollector(
-            model_reporters = {"Movimientos": movimientos_agentes,
-                                "% Celdas Limpias": calc_celdas_limpias,
-                                "Tiempo": calc_tiempo}
+            model_reporters = {"Movimientos": movimientosAgentes,
+                                "% Celdas Limpias": calcCeldasLimpias,
+                                "Tiempo": calcTiempo}
         )
     
     def step(self):
-        """Se avanza un paso en en modelo""" 
-        if(len(self.dirty_cells) > 0 and self.schedule.time <= self.time):
+        """
+        Función que avanza un paso en en modelo
+        Parámetros: 
+        self - modelo al que pertenece la función
+        Retorno:
+        Nada
+        """ 
+        if(len(self.dirtyCells) > 0 and self.schedule.time <= self.time):
             #Si todavía hay celdas sucias se continúa iterando
             self.datacollector.collect(self)
             self.schedule.step()
@@ -91,50 +117,84 @@ class LimpiadorModel(Model):
             self.datacollector.collect(self)
             self.running = False            
             print("Tiempo alcanzado: " + str(self.schedule.time))
-            print("Celdas faltantes: " + str(self.dirty_cells))
-            print("Porcentaje limpiado: " + str(calc_celdas_limpias(self)))
-            print("Movimientos totales: " + str(movimientos_agentes(self)))
+            print("Celdas faltantes: " + str(self.dirtyCells))
+            print("Porcentaje limpiado: " + str(calcCeldasLimpias(self)))
+            print("Movimientos totales: " + str(movimientosAgentes(self)))
 
 class AgenteLimpiador(Agent):
     """Agente que limpiará la celda"""
-    def __init__(self, unique_id, model, state):
+    def __init__(self, unique_id, model):
+        #Atributos
         super().__init__(unique_id, model)
-        self.celdas_limpiadas = 0
+        self.celdasLimpiadas = 0
         self.movimientos = 0
-        self.state = state  #0 -> limpiador; 1 -> suciedad; 2 -> limpio
     
     def aspirar(self, cell):
-        """Aspriar celda actual"""
+        """
+        Función que aspira celda actual
+        Parámetros: 
+        self - agente al que pertenece la función
+        cell - celda en la que se encuentra el agente
+        Retorno:
+        Nada
+        """
         #Se limpia la celda, es decir, se quita del arreglo de celdas sucias
-        self.celdas_limpiadas += 1
-        self.model.dirty_cells.remove(cell)
+        self.celdasLimpiadas += 1
+        self.model.dirtyCells.remove(cell)
     
     def move(self):
-        """Mover a una celda vecina aleatoria"""
+        """
+        Función que mueve al agente a una celda vecina aleatoria
+        Parámetros: 
+        self - agente al que pertenece la función
+        Retorno:
+        Nada
+        """
         #Se obtienen los vecinos del agente
         self.movimientos += 1
-        possible_steps = self.model.grid.get_neighborhood(
+        possibleSteps = self.model.grid.get_neighborhood(
             self.pos,
             moore = True,
             include_center = False
         )
         #Se escoge una posición aleatoria de entre los vecinos
-        new_pos = self.random.choice(possible_steps)
+        newPos = self.random.choice(possibleSteps)
         #Se mueve el agente
-        self.model.grid.move_agent(self, new_pos)
+        self.model.grid.move_agent(self, newPos)
 
     def step(self):
-        """Acciones que realizan los agentes en cada paso"""
-        #Se actualiza el tablero
-        if(self.pos not in self.model.dirty_cells and self.state == 1):
-            #Si el agente es suciedad y su celda ya fue limpiada se "limpia" del tablero
-            self.model.grid.remove_agent(self)
-            self.state = 2
-
-        if(self.pos in self.model.dirty_cells and self.state == 0):
+        """
+        Función que realiza ls acciones de cada agente en cada paso
+        Parámetros: 
+        self - agente al que pertenece la función
+        Retorno:
+        Nada
+        """
+        if(self.pos in self.model.dirtyCells):
             #Si la celda está sucia se aspira
             self.aspirar(self.pos)
         else:
             #De lo contrario el agente se mueve
-            if(self.state == 0):
-                self.move()
+            self.move()
+
+class Suciedad(Agent):
+    """Clase que modela la suciedad en el tablero"""
+    def __init__(self, unique_id, model):
+        #Atributos
+        super().__init__(unique_id, model)
+        self.dirty = True  #True -> la celda está sucia; False -> la celda está limpia 
+        self.movimientos = 0
+
+    def step(self):
+        """
+        Función que realiza ls acciones de cada agente en cada paso
+        Parámetros: 
+        self - agente al que pertenece la función
+        Retorno:
+        Nada
+        """
+        #Actualizar el tablero
+        if(self.pos not in self.model.dirtyCells and self.dirty):
+            #Si la celda ya no está sucia se "limpia" del tablero
+            self.model.grid.remove_agent(self)
+            self.dirty = False
